b3b4f21a190f15f3766ed394e9f15b0494f76885
Nicholas264 <22565232+Nicholas264@users.noreply.github.com>
2021-01-25 15:47:15 -0800

    There is no "1" in a deck of cards
    oops

--- src/Card.java
+++ src/Card.java
@@ -3,14 +3,14 @@ package src;
 public class Card {
 	
 	// indices in the array correspond to values of the ints in the card constructor
-	public static String[] values = {"1","2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+	public static String[] values = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 	public static String[] suits = {"♠","♥","♦","♣"};
 
 	int value;
 	int suit;
 
 	/**
-	 * @param value from 0 - 13
+	 * @param value from 0 - 12
 	 * @param suit from 0 - 3
 	 */
 	public Card(int value, int suit) {
